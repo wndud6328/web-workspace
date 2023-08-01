d50c029ea1110983c50fd034e57d6615f2bca46f
wndud6328 <133620265+wndud6328@users.noreply.github.com>
2023-08-01 12:57:16 +0900

    0801

---
+++ 02_HttpServletForm/src/servlet/form/HttpFormServlet.java
@@ -0,0 +1,50 @@
+package servlet.form;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HttpFormServlet
+ */
+public class HttpFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		/*
+		 * 한글처리..양방향!
+		 */
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		String id = request.getParameter("userId");
+		String password = request.getParameter("userPwd");
+		
+		String[] menuList = request.getParameterValues("menu");
+		String gender = request.getParameter("gender");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h2>정보를 출력합니다...</h2>");
+		out.println("<p>아이디 :" + id + "</p>");
+		out.println("<p>비밀번호 : " + password + "</p>");
+//		당신의 성별은 --> form.html 라디오 사용!
+		System.out.println(gender.charAt(0)=='M');
+		out.println("<p>성별 : " + (gender.charAt(0) == 'M' ? "남자" : "여자" ) +"</p>");
+		out.println("<ul>");
+		out.println("<h2>좋아하는 메뉴 !</h2>");
+		for(String menu : menuList) {
+			out.println("<li>" + menu + "</li>");
+		}
+		out.println("</ul>");
+		out.close();
+	}
+
+}
